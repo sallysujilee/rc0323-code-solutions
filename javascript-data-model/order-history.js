3bb5ecfc471237fecfd2153b73ee0758dbd6e205
Sally Lee <sallysujilee@gmail.com>
2023-03-21 18:36:05 +0000

    Completed javascript-data-model exercise.

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -12,7 +12,8 @@ var orderHistory = [
         title: 'JavaScript for impatient programmers',
         author: 'Rauschmayer, Dr. Axel',
         returnWindowClosedOn: 'Sep 7, 2020',
-        total: 31.55
+        total: 31.55,
+        imageUrl: 'images/js-book.png'
       }
     ]
   },
@@ -28,7 +29,8 @@ var orderHistory = [
         title: 'The Timeless Way of Building',
         author: 'Alexander, Christopher',
         returnWindowClosedOn: 'Aug 19, 2020',
-        total: 41.33
+        total: 41.33,
+        imageUrl: 'images/architecture-book.png'
       }
     ]
   },
@@ -43,7 +45,8 @@ var orderHistory = [
         type: 'hardware device',
         productName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Friver and No Lag-Gamecube Adapter',
         returnWindowClosedOn: 'Aug 5, 2020',
-        total: 15.98
+        total: 15.98,
+        imageUrl: 'images/gc-adapter.png'
       }
     ]
   },
@@ -58,7 +61,8 @@ var orderHistory = [
         type: 'hardware device',
         productName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
         returnWindowClosedOn: 'Aug 5, 2020',
-        total: 94.95
+        total: 94.95,
+        imageUrl: 'image/gc-controller.png'
       },
       {
         delivered: 'Jul 5, 2020',
@@ -66,7 +70,8 @@ var orderHistory = [
         title: 'The Art of Sql',
         author: 'Faroult, Stephane',
         returnWindowClosedOn: 'Aug 4, 2020',
-        total: 33.99
+        total: 33.99,
+        imagesUrl: 'images/book-sql.png'
       }
     ]
   }
