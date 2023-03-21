2f845c3c31bafc5062357f540a1c0a9cd730d7db
Sally Lee <sallysujilee@gmail.com>
2023-03-21 17:15:13 +0000

    Completed javascript-data-model exercise.

---
+++ javascript-data-model/order-history.js
@@ -0,0 +1,73 @@
+var orderHistory = [
+  {
+
+    orderPlaced: 'August 4, 2020',
+    total: 34.00,
+    shipTo: 'JS Masher',
+    orderNumber: '114-3941689-8772232',
+    item: [
+      {
+        delivered: 'Aug 8, 2020',
+        type: 'book',
+        title: 'JavaScript for impatient programmers',
+        author: 'Rauschmayer, Dr. Axel',
+        returnWindowClosedOn: 'Sep 7, 2020',
+        total: 31.55
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 19, 2020',
+    total: 44.53,
+    shipTo: 'JS Masher',
+    orderNumber: '113-9984268-1280257',
+    item: [
+      {
+        delivered: 'Jul 20, 2020',
+        type: 'book',
+        title: 'The Timeless Way of Building',
+        author: 'Alexander, Christopher',
+        returnWindowClosedOn: 'Aug 19, 2020',
+        total: 41.33
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 4, 2020',
+    total: 17.22,
+    shipTo: 'JS Masher',
+    orderNumber: '114-2875557-9059409',
+    item: [
+      {
+        delivered: 'Jul 7, 2020',
+        type: 'hardware device',
+        productName: 'Gamecube Controller Adapter. Super Smash Bros Switch Gamecube Adapter for WII U, PC. Support Turbo and Vibration Features. No Friver and No Lag-Gamecube Adapter',
+        returnWindowClosedOn: 'Aug 5, 2020',
+        total: 15.98
+      }
+    ]
+  },
+  {
+    orderPlaced: 'July 3, 2020',
+    total: 138.93,
+    shipTo: 'JS Masher',
+    orderNumber: '113-2883177-2648248',
+    item: [
+      {
+        delivered: 'Jul 5, 2020',
+        type: 'hardware device',
+        productName: 'GameCube Controller - Super Smash Bros. Edition (Nintendo Switch)',
+        returnWindowClosedOn: 'Aug 5, 2020',
+        total: 94.95
+      },
+      {
+        delivered: 'Jul 5, 2020',
+        type: 'book',
+        title: 'The Art of Sql',
+        author: 'Faroult, Stephane',
+        returnWindowClosedOn: 'Aug 4, 2020',
+        total: 33.99
+      }
+    ]
+  }
+];
