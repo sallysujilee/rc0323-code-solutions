09669056249137bb72bf310b4f3538aedcf02d91
Sally Lee <sallysujilee@gmail.com>
2023-04-03 18:39:12 +0000

    Completed dom-querying exercise.

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,19 +1,19 @@
 console.log('hello, world');
 
-const h1 = document.querySelector('h1');
-console.log(h1);
-console.dir(h1);
+const $heading = document.querySelector('h1');
+console.log('$heading', $heading);
+console.dir($heading);
 
-const explanation = document.querySelector('#explanation');
-console.log(explanation);
-console.dir(explanation);
+const $explanation = document.querySelector('#explanation');
+console.log('$explanation', $explanation);
+console.dir($explanation);
 
-const hint = document.querySelector('.hint');
-console.log(hint);
-console.dir(hint);
+const $hint = document.querySelector('.hint');
+console.log('$hint', $hint);
+console.dir($hint);
 
-const paragraphs = document.querySelectorAll('p');
-console.log(paragraphs);
+const $paragraphs = document.querySelectorAll('p');
+console.log('$paragraphs', $paragraphs);
 
-const exampleLinks = document.querySelectorAll('.example-link');
-console.log(exampleLinks);
+const $exampleLinks = document.querySelectorAll('.example-link');
+console.log('$exampleLinks', $exampleLinks);
