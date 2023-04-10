44fa1a2b415ce8c2536cce2dd97bcabbcc4bff86
Sally Lee <sallysujilee@gmail.com>
2023-04-10 12:20:36 +0000

    Completed javascript-light-bulb exercise.

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -3,13 +3,6 @@ const backLight = document.querySelector('body');
 
 button.addEventListener('click', yesClick);
 
-// function yesClick(event) {
-//   if (button.className === 'bulb-on') {
-//     button.className = 'bulb bulb-off';
-//   } else {
-//     button.className = 'bulb bulb-on';
-//   }
-
 function yesClick(event) {
   if (button.className === 'bulb bulb-off') {
     button.className = 'bulb bulb-half-on';
@@ -22,17 +15,3 @@ function yesClick(event) {
     backLight.className = 'dark-back-light';
   }
 }
-
-//   //  const bulbOff = '.bulb-off';
-//   // const bulbHalfOn = '.bulb-half-on';
-//   // const bulbOn = 'bulb-on';
-
-//   const bulbOff = document.getElementsByClassName('bulb');
-//   // const bulbHalfOn = document.getElementsByClassName('bulb-half-on bulb');
-//   // const bulbOn = document.getElementsByClassName('bulb-on bulb');
-
-//   console.log(bulbOff[0].className);
-
-//   bulbOff.className = 'bulb-half-on';
-//   // bulbHalfOn.className = 'bulb-on';
-//   // bulbOn.className = 'bulb-off';
