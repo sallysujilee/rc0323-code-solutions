c40ab05d737e9e15b97f90eb5b54c7e8f4f3804a
Sally Lee <sallysujilee@gmail.com>
2023-05-16 10:54:30 +0000

    Completed express-get-json exercise.

---
+++ express-get-json/server.js
@@ -0,0 +1,36 @@
+import express from 'express';
+
+const app = express();
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92
+  }
+};
+
+app.get('/api/grades', (req, res) => {
+  const gradesArray = [];
+  for (const grade in grades) {
+    gradesArray.push(grades[grade]);
+  }
+  return gradesArray;
+});
+
+app.listen(8080, () => {
+  console.log('Server running');
+});
