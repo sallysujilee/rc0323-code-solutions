5b95e072f239c155f6b20252e9ec11b9f6a57e3e
Sally Lee <sallysujilee@gmail.com>
2023-05-29 10:08:35 +0000

    fix later

---
+++ react-hot-button-component/my-app/src/HotButton.js
@@ -0,0 +1,30 @@
+import './HotButton.css';
+import { useState } from 'react';
+
+export default function HotButton() {
+  const [clicks, setClicks] = useState(0);
+
+  function handleClick() {
+    setClicks(clicks + 1);
+  }
+
+  function getTemp() {
+    if (clicks < 3) {
+      return 'cold'
+    } else if (clicks < 6) {
+      return 'cool'
+    } else if (clicks < 9) {
+      return 'tepid'
+    } else if (clicks < 12) {
+      return 'warm'
+    } else if (clicks < 15) {
+      return 'hot'
+    } else if (clicks < 18) {
+      return 'nuclear'
+    }
+  }
+
+  return (
+    <button onClick={handleClick} className={`hot-button` $}> Hot Button</button >
+  )
+}
